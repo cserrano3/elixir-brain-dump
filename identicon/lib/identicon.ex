2b191909ebe921515539c65274fb9aed9385fbce
carolina <you@example.com>
2020-02-26 21:25:15 -0300

    Initial project

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,45 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hashString
+    |> pickColor
+    |> buildGrid
+  end
+
+  def buildGrid(imageStruct) do
+
+    %Identicon.Image{ hex: hexList } = imageStruct
+    IO.inspect(hexList)
+    hexList
+    |> Enum.chunk_every(3, 3)
+    |> Enum.map(&mirrorRow/1) #defines reference to mirrorRow function with arity of one.
+  end
+
+  def mirrorRow(hexRow) do
+#    Identicon.main("ohh sweet love")
+#     HexGrid
+#    [
+#      [13, 77, 184],
+#      [56, 143, 224],
+#      [102, 190, 14],
+#      [211, 14, 231],
+#      [179, 90, 36],
+#      [231]
+#
+#      [13, 77, 184] must become [13,77,184,77,13]
+
+  [first, second | _tail] = hexRow
+  hexRow ++ [second, first] # ++ stands to append
+  end
+
+  def hashString(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+  # pattern matching is used to destructure the hexList variable from the imageStruct parameter
+  # yes, destructuring can also be done in the function signature
+  def pickColor( %Identicon.Image{ hex: [ red, green, blue | _tail ] } = imageStruct) do
+    %Identicon.Image{ imageStruct | color: { red, green, blue }}
+  end
+end
---
+++ identicon/lib/image.ex
@@ -0,0 +1,4 @@
+# Different from maps, you cannot add properties to the struct in runtime
+defmodule Identicon.Image do
+    defstruct hex: nil, color: nil
+end
